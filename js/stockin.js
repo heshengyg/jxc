9ea1ad85b342f3d3ae792c3bb346075cd3a6e9ff
heshengyg <15512469@qq.com>
2026-07-14 16:12:53 +0800

    f

--- js/stockin.js
+++ js/stockin.js
@@ -562,21 +562,19 @@ function exportStockInExcel(){
             return;
         }
 
-        let header = ["供应商","商品名称","规格","结算方式","销售单价","入库单价","入库数量","录入日期","生产日期","到期日期","发票状态","发票号码"];
+        let header = ["供应商","商品名称","规格","结算方式","销售单价","入库单价","入库数量","录入日期","生产日期","到期日期"];
         let expData = validData.map(item=>[
-            item.supplier||"",
-            item.goodsName||"",
-            item.spec||"",
-            item.settleType||"",
-            item.sale_price||0,
-            item.in_price||0,
-            item.in_num||0,
-            item.record_date||"",
-            item.produce_date||"",
-            item.expire_date||"",
-            item.invoice_status||"",
-            item.invoice_no||""
-        ]);
+    item.supplier||"",
+    item.goodsName||"",
+    item.spec||"",
+    item.settleType||"",
+    item.sale_price||0,
+    item.in_price||0,
+    item.in_num||0,
+    item.record_date||"",
+    item.produce_date||"",
+    item.expire_date||""
+]);
         
         console.log('📝 开始生成 Excel 工作簿');
         let ws = XLSX.utils.aoa_to_sheet([header,...expData]);
@@ -782,35 +780,25 @@ if (pageData.length > 0) {
                 `;
             }
             
-            let invoiceText = item.invoice_status || '';
-            let invoiceClass = '';
-            if (invoiceText === '未开票') {
-                invoiceClass = 'bg-yellow-invoice';
-            } else if (invoiceText === '已开票') {
-                invoiceClass = 'bg-green-invoice';
-            }
-            
             fullHtml += `
-                <tr>
-                    <td><input type="checkbox" class="in-item-checkbox" value="${item.id}" ${isUsed ? 'disabled' : ''}></td>
-                    <td>${start + idx + 1}</td>
-                    <td>${item.supplier || ''}</td>
-                    <td>${item.goodsName || ''}</td>
-                    <td>${item.spec || '-'}</td>
-                    <td>${item.settleType || ''}</td>
-                    <td>${formatMoney(item.in_price)}</td>
-                    <td>${item.in_num}</td>
-                    <td>${amount}</td>
-                    <td>${batchRemain}</td>
-                    <td>${totalStock}</td>
-                    <td class="${invoiceClass}">${invoiceText}</td>
-                    <td>${item.invoice_no || ''}</td>
-                    <td>${item.produce_date || ''}</td>
-                    <td>${item.expire_date || ''}</td>
-                    <td>${item.record_date || ''}</td>
-                    <td>${btnHtml}</td>
-                </tr>
-            `;
+    <tr>
+        <td><input type="checkbox" class="in-item-checkbox" value="${item.id}" ${isUsed ? 'disabled' : ''}></td>
+        <td>${start + idx + 1}</td>
+        <td>${item.supplier || ''}</td>
+        <td>${item.goodsName || ''}</td>
+        <td>${item.spec || '-'}</td>
+        <td>${item.settleType || ''}</td>
+        <td>${formatMoney(item.in_price)}</td>
+        <td>${item.in_num}</td>
+        <td>${amount}</td>
+        <td>${batchRemain}</td>
+        <td>${totalStock}</td>
+        <td>${item.produce_date || ''}</td>
+        <td>${item.expire_date || ''}</td>
+        <td>${item.record_date || ''}</td>
+        <td>${btnHtml}</td>
+    </tr>
+`;
         } catch (e) {
             console.error('渲染第', idx + 1, '行时出错:', e, pageData[idx]);
             // 继续渲染下一行
